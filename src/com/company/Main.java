8b452f3638cba63de73c57e029d4991842d7d628
yarik <yariks@i.ua>
2019-12-08 14:23:09 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
