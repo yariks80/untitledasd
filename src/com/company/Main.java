20b35b3a83a4e43b7ac4111ff2692ae2ad75cb29
yarik <yariks@i.ua>
2019-12-14 12:19:30 +0200

    fix44

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,5 +18,8 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
     }
 }
